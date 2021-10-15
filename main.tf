edb0f15773a84f39204251a4a4adf835f8c9031d
Cesar Rodriguez <cesar@accurics.com>
2021-10-15 08:55:27 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
-resource "aws_db_subnet_group" "km_rds_subnet_grp" {
-  name       = "km_rds_subnet_grp_${var.environment}"
-  subnet_ids = var.private_subnet
+#resource "aws_db_subnet_group" "km_rds_subnet_grp" {
+#  name       = "km_rds_subnet_grp_${var.environment}"
+#  subnet_ids = var.private_subnet
 
-  tags = merge(var.default_tags, {
-    Name = "km_rds_subnet_grp_${var.environment}"
-  })
-}
+#  tags = merge(var.default_tags, {
+#    Name = "km_rds_subnet_grp_${var.environment}"
+#  })
+#}
 
 resource "aws_security_group" "km_rds_sg" {
   name   = "km_rds_sg"
